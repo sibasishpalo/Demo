f82941b7b80e5a5c6e61ac9d7911b81d5c18f3b6
sibasishpalo <sibasish.palo@gmail.com>
2021-11-29 14:22:34 +0530

    Adding integration test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,44 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
 
+	@LocalServerPort
+	private int port;
+
+	TestRestTemplate restTemplate = new TestRestTemplate();
+
+	HttpHeaders headers = new HttpHeaders();
+
+
 	@Test
-	void contextLoads() {
+	public void testRetrieveStudent() throws Exception {
+		HttpEntity<String> entity = new HttpEntity<>(null, headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(
+				createURLWithPort("/test/query?reviewer=User1"), HttpMethod.GET, entity, String.class);
+
+		String expected = "{\n" +
+				"    \"title\": \"Sample Review Tile\",\n" +
+				"    \"reviewText\": \"Hello This is a sample review text\",\n" +
+				"    \"reviewer\": \"User1\"\n" +
+				"}";
+
+		JSONAssert.assertEquals(expected, response.getBody(), false);
+	}
+
+	private String createURLWithPort(String uri) {
+		return "http://localhost:" + port + uri;
 	}
 
 }
